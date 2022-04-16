143b5394511e31310825c1c6ec0221aabf284575
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-04-16 04:46:53 +0300

    unAuthMiddleware response update 401 to 403

--- src/middlewares/checkUnAuthMiddleware.js
+++ src/middlewares/checkUnAuthMiddleware.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if(req.isAuthenticated()){
-        res.status(401).send("Lütfen önce çıkış yapınız.")
+        res.status(403).send("Lütfen önce çıkış yapınız.")
     }else{
         next()
     }
